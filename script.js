eb10ccc53fcc3a94ea40a1affa5b51d100449342
ThePromiseIncarnate <ThePromiseIncarnate@gmail.com>
2024-08-10 11:39:32 -0500

    I am done

--- script.js
+++ script.js
@@ -6,11 +6,41 @@ function tooShort(firstName) {
     }
 }
 
+function hasAnI(firstName) {
+    let firstNameHasI = firstName;
+    if(firstNameHasI.includes("i")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 let marryEarlyElm = document.getElementById("marryEarly");
-let firstNameInput = document.getElementById("firstname");
+let marryNowElm = document.getElementById("marryNow");
+let firstNameInput = document.getElementById("firstName");
 
 function handleChangeInMarryEarly() {
     console.log("Hello world");
+    let input = firstName.value;
+    console.log(input);
+    let isShort = tooShort(input);
+    if (isShort) {
+        marryEarlyElm.style.color = "green";
+    } else {
+        marryEarlyElm.style.color = "red";
+    }
 }
 
-firstNameInput.addEventListener("change", handleChangeInMarryEarly)
+function handleChangeInI() {
+    let input = firstName.value;
+    console.log(input);
+    let hasI = hasAnI(input);
+    if (hasI) {
+        marryNowElm.style.color = "green";
+    } else {
+        marryNowElm.style.color = "red";
+    }
+}
+
+firstName.addEventListener("change", handleChangeInMarryEarly)
+firstName.addEventListener("change", handleChangeInI)
